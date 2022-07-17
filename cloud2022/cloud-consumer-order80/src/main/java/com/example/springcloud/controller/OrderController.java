e83982dc553af8dc3f1c6d2cb7b3ca749979442b
chengdashia <1847085602@qq.com>
2022-07-17 22:35:56 +0800

    ribbon 的负载均衡和rest调用

--- cloud2022/cloud-consumer-order80/src/main/java/com/example/springcloud/controller/OrderController.java
+++ cloud2022/cloud-consumer-order80/src/main/java/com/example/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.example.springcloud.controller;
 import com.example.springcloud.pojo.CommonResult;
 import com.example.springcloud.pojo.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +33,16 @@ public class OrderController {
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id, CommonResult.class);
     }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id){
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+        if(entity.getStatusCode().is2xxSuccessful()){
+            log.info(entity.getStatusCode()+"\t"+entity.getBody());
+            return entity.getBody();
+        }
+        return new CommonResult<>(444,"操作失败");
+    }
+
 
 }
