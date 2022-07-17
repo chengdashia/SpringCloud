09a5e50d4ec9800f7ed3eb5b8cf77c68380841e5
chengdashia <1847085602@qq.com>
2022-07-17 15:50:04 +0800

    Eureka 客户端安装

---
+++ cloud2022/cloud-eureka-server7001/src/main/java/com/example/springcloud/EurekaMain7001.java
@@ -0,0 +1,17 @@
+package com.example.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 成大事
+ * @since 2022/7/17 15:40
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class,args);
+    }
+}
