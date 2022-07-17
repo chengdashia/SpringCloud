f0ba3e03447d8013f0f7470429e7aac14961f9a6
chengdashia <1847085602@qq.com>
2022-07-17 16:09:52 +0800

    payment和consumer client端注册

--- cloud2022/cloud-provider-payment8001/src/main/java/com/example/springcloud/PaymentMain8001.java
+++ cloud2022/cloud-provider-payment8001/src/main/java/com/example/springcloud/PaymentMain8001.java
@@ -2,12 +2,14 @@ package com.example.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author 成大事
  * @since 2022/7/17 13:08
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
