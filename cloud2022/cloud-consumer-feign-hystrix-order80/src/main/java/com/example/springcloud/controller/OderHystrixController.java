aa1a551d8e1df694db1e9d61d24e150d7ace9b54
chengdashia <1847085602@qq.com>
2022-07-18 17:55:02 +0800

    hystrix 服务降级订单测fallback 80

--- cloud2022/cloud-consumer-feign-hystrix-order80/src/main/java/com/example/springcloud/controller/OderHystrixController.java
+++ cloud2022/cloud-consumer-feign-hystrix-order80/src/main/java/com/example/springcloud/controller/OderHystrixController.java
@@ -1,6 +1,8 @@
 package com.example.springcloud.controller;
 
 import com.example.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,8 +27,16 @@ public class OderHystrixController {
         return paymentHystrixService.paymentInfo_OK(id);
     }
 
+
     @GetMapping("/payment/hystrix/timeout/{id}")
+    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "1500")
+    })
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
         return paymentHystrixService.paymentInfo_TimeOut(id);
+    }
+
+    public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id) {
+        return "我是消费者80,对方支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,o(╥﹏╥)o";
     }
 }
