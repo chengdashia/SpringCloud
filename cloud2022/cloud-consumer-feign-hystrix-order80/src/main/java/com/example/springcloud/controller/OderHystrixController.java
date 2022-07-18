47271fd0b81a9fd91d8a6d3393d03d7f8eb05e2b
chengdashia <1847085602@qq.com>
2022-07-18 17:59:48 +0800

    hystrix 全局服务降级DefaultProperties

--- cloud2022/cloud-consumer-feign-hystrix-order80/src/main/java/com/example/springcloud/controller/OderHystrixController.java
+++ cloud2022/cloud-consumer-feign-hystrix-order80/src/main/java/com/example/springcloud/controller/OderHystrixController.java
@@ -1,8 +1,8 @@
 package com.example.springcloud.controller;
 
 import com.example.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +18,7 @@ import javax.annotation.Resource;
 @Slf4j
 @RestController
 @RequestMapping("/consumer")
+@DefaultProperties(defaultFallback = "paymentGlobalFallBackMethod")
 public class OderHystrixController {
     @Resource
     private PaymentHystrixService paymentHystrixService;
@@ -27,16 +28,23 @@ public class OderHystrixController {
         return paymentHystrixService.paymentInfo_OK(id);
     }
 
-
     @GetMapping("/payment/hystrix/timeout/{id}")
-    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "1500")
-    })
+    //@HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
+    //        @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "1500")
+    //})
+    @HystrixCommand     //用全局的fallback方法
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
         return paymentHystrixService.paymentInfo_TimeOut(id);
     }
 
     public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id) {
         return "我是消费者80,对方支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,o(╥﹏╥)o";
+    }
+
+    /**
+     *下面是全局fallback方法
+     */
+    public String paymentGlobalFallBackMethod(){
+        return "Global异常处理信息，请稍后再试，/(ㄒoㄒ)/~~";
     }
 }
