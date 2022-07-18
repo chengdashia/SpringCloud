f930ab7a7558ca261f7d1b2d92b3dfae02ec0cf8
chengdashia <1847085602@qq.com>
2022-07-18 21:34:05 +0800

    gateway9527 网关搭建

---
+++ cloud2022/cloud-gateway-gateway9527/src/main/java/com/example/springcloud/GateWayMain9527.java
@@ -0,0 +1,17 @@
+package com.example.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author 成大事
+ * @since 2022/7/18 21:16
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
