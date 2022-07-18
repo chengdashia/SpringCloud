95ac41228d80fa1dc7b32094bc520ba4e855b223
chengdashia <1847085602@qq.com>
2022-07-18 20:27:31 +0800

    HystrixDashboard

---
+++ cloud2022/cloud-consumer-hystrix-dashboard9001/src/main/java/com/example/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,17 @@
+package com.example.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author 成大事
+ * @since 2022/7/18 20:22
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class,args);
+    }
+}
