dbc39fdd5730a652fd741ab6bda6474ab005299d
chengdashia <1847085602@qq.com>
2022-07-19 15:32:13 +0800

    消费驱动消费者

---
+++ cloud2022/cloud-stream-rabbitmq-consumer8802/src/main/java/com/example/springcloud/StreamMQMain8802.java
@@ -0,0 +1,15 @@
+package com.example.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 成大事
+ * @since 2022/7/19 15:21
+ */
+@SpringBootApplication
+public class StreamMQMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class,args);
+    }
+}
