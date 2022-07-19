27113022202fc139b14d3dcb12a0b522c834abc9
chengdashia <1847085602@qq.com>
2022-07-19 13:02:53 +0800

    config 客户端搭建

---
+++ cloud2022/cloud-config-client-3355/src/main/java/com/example/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,20 @@
+package com.example.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 成大事
+ * @since 2022/7/19 12:53
+ */
+@RestController
+public class ConfigClientController{
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
