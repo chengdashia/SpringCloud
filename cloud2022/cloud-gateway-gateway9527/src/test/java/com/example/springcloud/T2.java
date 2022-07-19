0f088720bdb1459c3afd730e2df2bd6edbc9d642
chengdashia <1847085602@qq.com>
2022-07-19 11:56:07 +0800

    predicate and filter

---
+++ cloud2022/cloud-gateway-gateway9527/src/test/java/com/example/springcloud/T2.java
@@ -0,0 +1,16 @@
+package com.example.springcloud;
+
+import java.time.ZonedDateTime;
+
+/**
+ * @author 成大事
+ * @since 2022/7/19 11:11
+ */
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);
+
+        //2022-07-19T11:12:00.736+08:00[Asia/Shanghai]
+    }
+}
