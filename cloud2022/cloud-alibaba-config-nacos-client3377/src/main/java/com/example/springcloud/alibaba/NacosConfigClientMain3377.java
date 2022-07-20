587436378a4928106a50322cbd2fc0c060bd0d60
chengdashia <1847085602@qq.com>
2022-07-20 14:12:57 +0800

    nacos 服务配置中心

---
+++ cloud2022/cloud-alibaba-config-nacos-client3377/src/main/java/com/example/springcloud/alibaba/NacosConfigClientMain3377.java
@@ -0,0 +1,17 @@
+package com.example.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author 成大事
+ * @since 2022/7/20 13:33
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigClientMain3377{
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
