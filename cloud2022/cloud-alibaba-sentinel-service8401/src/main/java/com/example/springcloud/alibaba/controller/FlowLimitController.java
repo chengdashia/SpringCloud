be953ed07176459b6e293491567ca4160de50058
chengdashia <1847085602@qq.com>
2022-07-20 19:46:34 +0800

    sentinel 降级

--- cloud2022/cloud-alibaba-sentinel-service8401/src/main/java/com/example/springcloud/alibaba/controller/FlowLimitController.java
+++ cloud2022/cloud-alibaba-sentinel-service8401/src/main/java/com/example/springcloud/alibaba/controller/FlowLimitController.java
@@ -4,15 +4,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author 成大事
  * @since 2022/7/20 18:00
  */
-@RestController
 @Slf4j
+@RestController
 public class FlowLimitController {
     @GetMapping("/testA")
     public String testA(){
+        log.info(Thread.currentThread().getName()+"\t"+"...testA");
         return "------testA";
     }
 
@@ -20,5 +23,30 @@ public class FlowLimitController {
     public String testB(){
         log.info(Thread.currentThread().getName()+"\t"+"...testB");
         return "------testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD() {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("testD 测试RT");
+        return "------testE";
+    }
+
+    @GetMapping("/testE")
+    public String testE() {
+        log.info("testE 异常比例");
+        int age = 10/0;
+        return "------testD";
+    }
+
+    @GetMapping("/testF")
+    public String testF() {
+        log.info("testF 异常数");
+        int age = 10/0;
+        return "------testF 测试异常数";
     }
 }
