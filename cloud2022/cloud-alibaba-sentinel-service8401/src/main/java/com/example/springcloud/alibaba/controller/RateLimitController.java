135345a98154cfdf59ac17146445af7d86749875
chengdashia <1847085602@qq.com>
2022-07-20 21:34:57 +0800

    sentinelResource 配置

---
+++ cloud2022/cloud-alibaba-sentinel-service8401/src/main/java/com/example/springcloud/alibaba/controller/RateLimitController.java
@@ -0,0 +1,33 @@
+package com.example.springcloud.alibaba.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.example.springcloud.pojo.CommonResult;
+import com.example.springcloud.pojo.Payment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 成大事
+ * @since 2022/7/20 21:24
+ */
+@RestController
+public class RateLimitController{
+
+    @GetMapping("/byResource")
+    @SentinelResource(value = "byResource",blockHandler = "handleException")
+    public CommonResult byResource() {
+        return new CommonResult(200,"按资源名称限流测试OK",new Payment(2020L,"serial001"));
+    }
+
+
+    public CommonResult handleException(BlockException exception) {
+        return new CommonResult(444,exception.getClass().getCanonicalName()+"\t 服务不可用");
+    }
+
+    @GetMapping("/rateLimit/byUrl")
+    @SentinelResource(value = "byUrl")
+    public CommonResult byUrl(){
+        return new CommonResult(200,"按url限流测试OK",new Payment(2020L,"serial002"));
+    }
+}
