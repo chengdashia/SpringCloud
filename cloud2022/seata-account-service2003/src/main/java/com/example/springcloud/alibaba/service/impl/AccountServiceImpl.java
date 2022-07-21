d00edf72610f2bd7fcf94c6370efc7a10abeb413
chengdashia <1847085602@qq.com>
2022-07-21 14:25:36 +0800

    超时异常，没加@GlobalTransactional

--- cloud2022/seata-account-service2003/src/main/java/com/example/springcloud/alibaba/service/impl/AccountServiceImpl.java
+++ cloud2022/seata-account-service2003/src/main/java/com/example/springcloud/alibaba/service/impl/AccountServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author 成大事
@@ -28,6 +29,9 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public void decrease(Long userId, BigDecimal money) {
         LOGGER.info("------->account-service中扣减账户余额开始");
+        //模拟超时异常，全局事务回滚
+        //暂停几秒钟线程
+        try { TimeUnit.SECONDS.sleep(20); } catch (InterruptedException e) { e.printStackTrace(); }
         accountDao.decrease(userId,money);
         LOGGER.info("------->account-service中扣减账户余额结束");
     }
